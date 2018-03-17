2c3ff3047b5ffbbace0f77628043519b3b629852
MaxMcCool <selmoro@hotmail.com>
2018-03-17 11:41:49 -0400

    Unity Networking Tutorial
    Completed Unity Networking Tutorial Project

---
+++ MultiplayerSystemsNetworkingProject/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<HealthScript> ();
+		if (health != null) {
+			health.TakeDamage (10);
+		}
+
+		Destroy (gameObject);
+	}
+}
